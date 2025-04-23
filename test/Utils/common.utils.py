bdc8e8f57977a79459b09934971417bb40035aa5
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-23 10:23:25 +0530

    23th April 2025

---
+++ test/Utils/common.utils.py
@@ -0,0 +1,24 @@
+from aenum import constant
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import Constants
+
+# method overloading
+
+def webdriver_wait(driver, element_tuple):
+
+
+    WebDriverWait(driver=driver, timeout=5).until(
+        EC.visibility_of_element_located(element_tuple))
+
+
+def webdriver_wait(driver, element_tuple, timeout):
+
+
+    WebDriverWait(driver=driver, timeout=timeout).until(
+        EC.visibility_of_element_located(element_tuple))
+
+
+def webdriver_wait_url(driver, timeout):
+    WebDriverWait(driver=driver, timeout=timeout).until(
+        EC.url_changes(Constants.app_dashboard_url()))
---
+++ test/pageobjects/PageObjectModal/vwo/loginPage.py
@@ -0,0 +1,49 @@
+# Login page
+
+from selenium.webdriver.common.by import By
+from test.Utils.common_Utils import webdriver_wait
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        # Page Locators
+
+        username = (By.ID, "login-username")
+        password = (By.NAME, "password")
+        submit_button = (By.XPATH, "//button[@id='js-login-btn']")
+        error_message = (By.CSS_SELECTOR, "#js-notification-box-msg")
+
+        # Remove them if you are not using them as of now
+        # free trial = (By.XPATH , "//a[normalize-space()='start a free trial']")
+        # forgot_password_button = (By.XPATH, "//.XPATH, "//button[normalize-space()='Forgot password']")
+        # sso_login = (By.XPATH, "//button[normalize-space()='sign in using sso']")
+        # remember_checkbox = (By.XPATH, "//label[@for='checkbox-remember']//span[@class=
+
+        # page actions
+
+    def get_username(self):
+            return self.driver.find_element(*LoginPage.username)
+
+    def get_password(self):
+            return self.driver.find_element(*LoginPage.password)
+
+    def get_submit_button(self):
+            return self.driver.find_element(*LoginPage.submit_button)
+
+    def get_error_message(self):
+            webdriver_wait(driver=self.driver, element_tuple=self.error_message, timeout=5)
+            return self.driver.find_element(*LoginPage.error_message)
+
+    def login_to_vwo(self, usr, pwd):
+            try:
+                self.get_username().send_keys(usr)
+                self.get_username().send_keys(pwd)
+                self.get_submit_button().click()
+            except Exception as e:
+                print(e)
+
+    def get_error_message_text(self):
+                    return self.get_error_message().text
